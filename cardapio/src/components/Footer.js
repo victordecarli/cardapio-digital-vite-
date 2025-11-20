58dc01a403fd720d1c63997f30484253afb320e4
Victor de Carli <victordecarliJAODELIVERY@MacBook-Air-de-Suporte.local>
2025-11-19 21:01:42 -0400

    feat: adiciona navbar e footer components

---
+++ cardapio/src/components/Footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+  return `
+        <footer class="footer">
+                    <span class="footer-span">2025 - Todos direitos reservados</span>
+        </footer>
+    `;
+}
---
+++ cardapio/src/navbar.js
@@ -0,0 +1,17 @@
+
+export function Navbar(currentPage = 'home') {
+    return `
+        <nav class="navbar">
+            <div class="navbar-logo">
+                <img src="../assets/logo.png" alt="Logo" class="logo" />
+            </div>
+            <ul class="navbar-links">
+                <li><a href="#/" class="${currentPage === 'home' ? 'active' : ''}">Início</a></li>
+                <li><a href="#/cardapio" class="${currentPage === 'cardapio' ? 'active' : ''}">Cardápio</a></li>
+                <li><a href="#/sobre" class="${currentPage === 'sobre' ? 'active' : ''}">Sobre</a></li>
+                <li><a href="#/localizacao" class="${currentPage === 'localizacao' ? 'active' : ''}">Localização</a></li>
+                <li><a href="#/contato" class="${currentPage === 'contato' ? 'active' : ''}">Contato</a></li>
+            </ul>
+        </nav>
+    `;
+}
