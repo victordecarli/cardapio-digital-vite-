865f2b61247b4c73af4ae4bf3eb58da2a98d5dfb
Victor de Carli <victordecarliJAODELIVERY@MacBook-Air-de-Suporte.local>
2025-11-25 09:04:10 -0400

    refactor: remove rota nao utilizada

--- cardapio/src/navbar.js
+++ cardapio/src/navbar.js
@@ -8,7 +8,6 @@ export function Navbar(currentPage = 'home') {
             <ul class="navbar-links">
                 <li><a href="#/" class="${currentPage === 'home' ? 'active' : ''}">Início</a></li>
                 <li><a href="#/cardapio" class="${currentPage === 'cardapio' ? 'active' : ''}">Cardápio</a></li>
-                <li><a href="#/sobre" class="${currentPage === 'sobre' ? 'active' : ''}">Sobre</a></li>
                 <li><a href="#/localizacao" class="${currentPage === 'localizacao' ? 'active' : ''}">Localização</a></li>
                 <li><a href="#/contato" class="${currentPage === 'contato' ? 'active' : ''}">Contato</a></li>
             </ul>
