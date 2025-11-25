de72a1c480c374d8b7401f4e01c5f1ca710a6561
Victor de Carli <victordecarliJAODELIVERY@MacBook-Air-de-Suporte.local>
2025-11-25 09:04:43 -0400

    feat: adiciona novas informacoes na tela de cardapio

--- cardapio/src/pages/Cardapio.js
+++ cardapio/src/pages/Cardapio.js
@@ -1,27 +1,398 @@
 export function CardapioPage() {
   return `
-        <section class="cardapio">
-            <div class="container">
-                <h1>Nosso Cardápio</h1>
-                <p>Confira nossos deliciosos pratos!</p>
+       <section class="cardapio" id="cardapio">
+    <div class="menu">
 
-                <div class="menu-categories">
-                    <h2>Entradas</h2>
-                    <div class="menu-items">
-                        <p>Em breve...</p>
-                    </div>
+      <h1>Nosso<span> Cardápio</span></h1>
 
-                    <h2>Pratos Principais</h2>
-                    <div class="menu-items">
-                        <p>Em breve...</p>
-                    </div>
 
-                    <h2>Sobremesas</h2>
-                    <div class="menu-items">
-                        <p>Em breve...</p>
-                    </div>
-                </div>
+
+
+      <div class="menu_box">
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Hamburguer</h2>
+
+            <p>Lanche artesanal com diversos sabores, preparado com ingredientes frescos. Venha
+              experimentar!
+            </p>
+
+            <h3>R$25.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
             </div>
-        </section>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Cheeseburguer Duplo</h2>
+
+            <p>Dois suculentos hambúrgueres, queijo derretido e molho especial para os amantes de sabor.
+            </p>
+
+            <h3>R$32.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Batata Frita Grande</h2>
+
+            <p>Porção crocante e dourada, perfeita para acompanhar seu lanche favorito</p>
+
+            <h3>R$30.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Frango Frito</h2>
+
+            <p>Frango crocante por fora e macio por dentro, temperado no ponto certo.</p>
+
+            <h3>R$28.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Pizza Média</h2>
+
+            <p>Massa artesanal, recheio caprichado e queijo derretido em cada fatia.</p>
+
+            <h3>R$40.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Pastel </h2>
+
+            <p>Tradicional e recheado na medida certa, disponível nos sabores carne e queijo.</p>
+
+            <h3>R$ 8,00 cada</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Super Burguer</h2>
+
+            <p>Um lanche completo: hambúrguer duplo, bacon, queijo, salada e molho especial.</p>
+
+            <h3>R$42.00</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+        <div class="menu_card">
+
+          <div class="menu_image">
+
+            <img src="../assets/Image-not-found.png">
+
+          </div>
+
+          <div class="small_card">
+
+            <i class="fa-solid fa-heart"></i>
+
+          </div>
+
+          <div class="menu_info">
+
+            <h2>Refrigerantes</h2>
+
+            <p>Variedade de sabores gelados para acompanhar sua refeição</p>
+
+            <h3>R$10.00 cada</h3>
+
+            <div class="menu_icon">
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star "></i>
+
+              <i class="fa-solid fa-star-half-stroke "></i>
+
+            </div>
+
+            <a href="#" class="menu_btn">Peça agora</a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="local">
+
+        <h1>Venha nos <span>Visitar!</span></h1>
+
+        <div class="local-content">
+
+          <img src="../assets/Image-not-found.png">
+
+          <p>
+
+            Cacoal Shopping<br>
+
+            R. Antônio Deodato Durce, 3500 - Floresta, Cacoal - RO, 76963-862<br>
+
+            Tel: (69) 9 9987-6540 / (69) 9 8485-7873<br>
+
+            E-mail: <a href="mailto:contatorest@gmail.com">contatorest@gmail.com</a>
+
+            <br>Horário de funcionamento: Todos os dias dás 10h as 22h<br>
+
+          </p>
+
+        </div>
+
+
+
+      </div>
+
+    </div>
+    </div>
+  </section>
     `;
 }
